f35c9b381926441ab249456fe807143193b0547c
Sasindu Nimsara <snimsara456@gmail.com>
2026-06-17 14:39:23 +0530

    Revert "Add files via upload"

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE Users (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Username NVARCHAR(50) NOT NULL,
-    Password NVARCHAR(50) NOT NULL
-);
-
-INSERT INTO Users (Username, Password) 
-VALUES ('admin', '1234');
